839b31139c46d036e73aa1d3ca33cc0207b67c7d
Bryant C <bcong@crai.com>
2026-07-13 09:45:11 -0500

    feat: add option of magic to execute cells

--- ipynb_to_py.py
+++ ipynb_to_py.py
@@ -1,15 +1,20 @@
-import nbformat
 from pathlib import Path
 
-def nb_to_py(path):
+import nbformat
+
+
+def nb_to_py(path: str, add_cell_markers: bool = False) -> None:
     nb = nbformat.read(path, as_version=4)
-    code = "\n\n".join(
-        cell.source
-        for cell in nb.cells
-        if cell.cell_type == "code"
-    )
+    cells = [cell.source for cell in nb.cells if cell.cell_type == "code"]
+
+    if add_cell_markers:
+        code = "\n\n".join(f"# %%\n{cell}" for cell in cells)
+    else:
+        code = "\n\n".join(cells)
+
     with open(path.replace(".ipynb", ".py"), "w") as f:
         f.write(code)
 
+
 for nb_path in Path(".").glob("**/*.ipynb"):
-    nb_to_py(str(nb_path))
+    nb_to_py(str(nb_path), add_cell_markers=True)
